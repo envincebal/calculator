72810b5541a11cd9122d614190468af9b0952ab1
Vincent Yan <vincentyan1@gmail.com>
2017-06-12 15:30:03 -0700

    Added separate event handlers for backspace and clear buttons

--- js/app.js
+++ js/app.js
@@ -1,23 +1,20 @@
 var display = document.querySelector(".result");
 var number = document.getElementsByClassName("number-button");
-var clear = document.getElementsByClassName("clear-button");
 var operator = document.getElementsByClassName("operator-button");
 var equals = document.querySelector(".equals-button");
+var clear = document.querySelector(".all-clear");
+var backSpace = document.querySelector(".backspace");
 var state = 0;
 
-function reset(){
+function reset() {
 	display.value = "";
-}
-
-function backSpace(){
-	display.value = display.value.slice(0, -1);
 }
 
 for (var i = 0; i < number.length; i++) {
 	number[i].addEventListener("click", function(){
-		if (state === 0) {
+		if (state === 0){
 			display.value += parseInt(this.value, 10);
-		} else if (state === 1) {
+		} else if (state === 1){
 			reset();
 			display.value += parseInt(this.value, 10);
 			state = 0;
@@ -27,7 +24,7 @@ for (var i = 0; i < number.length; i++) {
 
 for (var i = 0; i < operator.length; i++) {
 	operator[i].addEventListener("click", function(){
-		if (display.value.substring(display.value.length - 1) === this.value) {
+		if (display.value.substring(display.value.length - 1) === this.value){
 			this.value += "";
 		} else if (display.value === "" || display.value.slice(this.value) || state === 1){
 			display.value += this.value;
@@ -36,16 +33,13 @@ for (var i = 0; i < operator.length; i++) {
 	})
 }
 
-for (var i = 0; i < clear.length; i++) {
-	clear[i].addEventListener("click", function(){
-		if (this.value === "AC") {
-			reset();
-		}; 
-		if (this.value === "CE") {
-			backSpace();
-		};
-	})
-}
+clear.addEventListener("click", function(){
+	reset();
+})	
+
+backSpace.addEventListener("click", function(){
+	display.value = display.value.slice(0, -1);
+})
 
 equals.addEventListener("click", function(){
 	display.value = eval(display.value);
