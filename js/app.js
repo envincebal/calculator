074044028975c877403ec39f4ef8e652cc6ceb41
Vincent Yan <vincentyan1@gmail.com>
2017-12-13 23:17:22 -0800

    Fixed bug that stacks operators next to each other.

--- js/app.js
+++ js/app.js
@@ -1,49 +1,53 @@
-var display = document.querySelector(".result");
-var number = document.getElementsByClassName("number-button");
-var operator = document.getElementsByClassName("operator-button");
-var equals = document.querySelector(".equals-button");
-var clear = document.querySelector(".all-clear");
-var backSpace = document.querySelector(".backspace");
-var state = 0;
+const display = document.querySelector(".result");
+const number = document.getElementsByClassName("number-button");
+const operator = document.getElementsByClassName("operator-button");
+const zero = document.querySelector(".zero-button");
+const equals = document.querySelector(".equals-button");
+const clear = document.querySelector(".all-clear");
+const backSpace = document.querySelector(".backspace");
+let state = 0;
 
 function reset() {
 	display.value = "";
 }
 
 for (var i = 0; i < number.length; i++) {
-	number[i].addEventListener("click", function(){
-		if (state === 0){
+	number[i].addEventListener("click", function () {
+
+		if (state === 0) {
 			display.value += parseInt(this.value, 10);
-		} else if (state === 1){
+		} else if (state === 1) {
 			reset();
 			display.value += parseInt(this.value, 10);
 			state = 0;
 		}
+
 	})
 }
 
 for (var i = 0; i < operator.length; i++) {
-	operator[i].addEventListener("click", function(){
-		if (display.value.substring(display.value.length - 1) === this.value){
-			this.value += "";
-		} else if (display.value === "" || display.value.slice(this.value) || state === 1){
+	operator[i].addEventListener("click", function () {
+		const prevIndex = display.value.substring(display.value.length- 1);
+
+		if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/" || prevIndex === ".") {
+			display.value = display.value.slice(0, -1);
+			display.value += this.value;
+		} else if (display.value.length > 0) {
 			display.value += this.value;
 			state = 0;
 		}
 	})
 }
 
-clear.addEventListener("click", function(){
+clear.addEventListener("click", function () {
 	reset();
-})	
+})
 
-backSpace.addEventListener("click", function(){
+backSpace.addEventListener("click", function () {
 	display.value = display.value.slice(0, -1);
 })
 
-equals.addEventListener("click", function(){
+equals.addEventListener("click", function () {
 	display.value = eval(display.value);
 	state = 1;
 })
-
-
