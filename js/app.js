0bb733c544afff9085adffef9cb7b94bdbe2ac41
envincebal <vincentyan1@gmail.com>
2018-05-19 16:56:22 -0700

    Added digit limit and message for bad syntax

--- js/app.js
+++ js/app.js
@@ -13,13 +13,30 @@
 
 	function reset() {
 		display.value = "";
+		newNumber = false;
 		decimalState = true;
 		zeroState = false;
+	}
+
+	// Sets digit limit on display to 10 numbers. Function discounts decimal from number limit
+	function digitLimit() {
+		if (display.value.includes(".")) {
+			if (display.value.length > 11) {
+				display.value = "Digit Limit Met";
+				newNumber = true;
+			}
+		} else {
+			if (display.value.length > 10) {
+				display.value = "Digit Limit Met";
+				newNumber = true;
+			}
+		}
 	}
 
 	// Iterates through number elements and sets event listeners
 	for (var i = 0; i < number.length; i++) {
 		number[i].addEventListener("click", function () {
+			let number = parseInt(display.value);
 			if (newNumber === false) {
 				display.value += parseInt(this.value, 10);
 			} else if (newNumber === true) {
@@ -28,13 +45,14 @@
 				newNumber = false;
 			}
 			zeroState = true;
+			digitLimit();
 		});
 	}
 
 	// Iterates through operator elements and sets event listeners
 	for (var i = 0; i < operator.length; i++) {
 		operator[i].addEventListener("click", function () {
-			const prevIndex = display.value.substring(display.value.length - 1);
+			const prevIndex = display.value.slice(-1);
 
 			// if previous value is an operator, a new clicked operator will replace it
 			if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/") {
@@ -46,6 +64,7 @@
 			}
 			decimalState = true;
 			zeroState = false;
+			digitLimit();
 		});
 	}
 
@@ -72,19 +91,30 @@
 		} else {
 			display.value += "";
 		}
+		digitLimit();
 	});
 
 	zero.addEventListener("click", function () {
+		// Prevents first number between operators from being zero
 		if (!zeroState) {
 			display.value += "";
 		} else {
 			display.value += this.value;
 		}
+		digitLimit();
 	})
 
 	// Evaluates expression on display and rounds to the nearest hundredths if a decimal
 	equals.addEventListener("click", function () {
-		display.value = parseFloat(eval(display.value).toFixed(2));
-		decimalState = true;
+		const lastDigit = display.value.slice(-1);
+		// If last digit is not a number then "Bad Syntax!" will display otherwise it will evaluate expression
+		if (isNaN(lastDigit)) {
+			display.value = "Bad Syntax!";
+			newNumber = true;
+		} else {
+			// Rounds any decimal total to nearest hundredth
+			display.value = parseFloat(eval(display.value).toFixed(2));
+			decimalState = true;
+		}
 	});
 })()
