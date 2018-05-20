d4029fc04c5979389768cc7858b08f8035023e75
Vincent Yan <vincentyan1@gmail.com>
2018-05-20 13:48:03 -0700

    fixed operator bug that erases history

--- js/app.js
+++ js/app.js
@@ -1,152 +1,153 @@
 (function () {
-	const display = document.querySelector(".result");
-	const history = document.querySelector(".history");
-	const number = document.getElementsByClassName("number-button");
-	const operator = document.getElementsByClassName("operator-button");
-	const zero = document.querySelector(".zero-button");
-	const equals = document.querySelector(".equals-button");
-	const clear = document.querySelector(".all-clear");
-	const backSpace = document.querySelector(".backspace");
-	const decimal = document.querySelector(".decimal-button");
-	let newNumber = false;
-	let decimalState = true;
-	let zeroState = false;
+  const display = document.querySelector(".result");
+  const history = document.querySelector(".history");
+  const number = document.getElementsByClassName("number-button");
+  const operator = document.getElementsByClassName("operator-button");
+  const zero = document.querySelector(".zero-button");
+  const equals = document.querySelector(".equals-button");
+  const clear = document.querySelector(".clear-button");
+  const decimal = document.querySelector(".decimal-button");
+  let newNumber = false;
+  let decimalState = true;
+  let zeroState = false;
 
-	init();
+  init();
 
-	function init() {
-		display.textContent = "0";
-		history.textContent = "0";
-		newNumber = false;
-		decimalState = true;
-		zeroState = false;
-	}
+  function init() {
+    display.textContent = "0";
+    history.textContent = "0";
+    newNumber = false;
+    decimalState = true;
+    zeroState = false;
+  }
 
-	// Sets digit limit on display to 10 numbers. Function discounts decimal from number limit
-	function digitLimit() {
-		if (display.textContent.includes(".")) {
-			if (display.textContent.length > 11) {
-				display.textContent = "Digit Limit Met";
-				history.textContent = "";
+  function digitLimit() {
+    // Sets digit limit on display to 10 characters. Function discounts decimal from
+    // number limit
+    if (display.textContent.includes(".")) {
+      if (display.textContent.length > 11) {
+        display.textContent = "Digit Limit Met";
+        history.textContent = "";
+      }
+    } else {
+      if (display.textContent.length > 10) {
+        display.textContent = "Digit Limit Met";
+        history.textContent = "";
+      }
+    }
+    // Sets digit limit on history to 32 characters.
+    if (history.textContent.length > 32) {
+      display.textContent = "Digit Limit Met";
+      history.textContent = "";
+    }
+  }
 
-			}
-		} else {
-			if (display.textContent.length > 10) {
-				display.textContent = "Digit Limit Met";
-				history.textContent = "";
+  // Iterates through number elements and sets event listeners
+  for (var i = 0; i < number.length; i++) {
+    number[i]
+      .addEventListener("click", function () {
+        const prevDigit = display
+          .textContent
+          .slice(-1);
+        if (display.textContent.length === 1 && display.textContent === "0") {
+          display.textContent = this.value;
+          history.textContent = this.value;
+        } else if (isNaN(prevDigit) && prevDigit !== ".") {
+          display.textContent = this.value;
+          history.textContent += this.value;
+        } else {
+          display.textContent += this.value;
+          history.textContent += this.value;
+        }
+        zeroState = true;
+        digitLimit();
+      });
+  }
 
-			}
-		}
+  // Iterates through operator elements and sets event listeners
+  for (var i = 0; i < operator.length; i++) {
+    operator[i]
+      .addEventListener("click", function () {
+        const prevIndex = display
+          .textContent
+          .slice(-1);
+        // if previous value is an operator, a new clicked operator will replace it
+        if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/") {
+          display.textContent = display
+            .textContent
+            .slice(0, -1);
+          history.textContent = history
+            .textContent
+            .slice(0, -1);
+          display.textContent += this.value;
+          history.textContent += this.value;
+        } else if (history.textContent.length > 0) {
+          display.textContent = this.value;
+          history.textContent += this.value;
+        }
+        decimalState = true;
+        zeroState = false;
+        digitLimit();
+      });
+  }
 
-		if (history.textContent.length > 32) {
-			display.textContent = "Digit Limit Met";
-			history.textContent = "";
+  // clears display value
+  clear
+    .addEventListener("click", function () {
+      init();
+    });
 
-		}
-	}
+  // adds decimal to display
+  decimal.addEventListener("click", function () {
+    const lastDigit = display
+      .textContent
+      .slice(-1);
+    // if decimal button is pressed, then it is disabled unless decimal in current
+    // integer is deleted
+    if (decimalState && isNaN(lastDigit)) {
+      display.textContent = "0" + this.value;
+      history.textContent += "0" + this.value;
+      decimalState = false;
+      zeroState = true;
+    } else if (decimalState) {
+      display.textContent += this.value;
+      history.textContent += this.value;
+      decimalState = false;
+      zeroState = true;
+    } else {
+      display.textContent += "";
+    }
+    digitLimit();
+  });
 
+  zero.addEventListener("click", function () {
+    // Prevents first number between operators from being zero
+    if (!zeroState) {
+      display.textContent += "";
+    } else {
+      display.textContent += this.value;
+      history.textContent += this.value;
+    }
+    digitLimit();
+  })
 
-	// Iterates through number elements and sets event listeners
-	for (var i = 0; i < number.length; i++) {
-		number[i].addEventListener("click", function () {
-			const prevDigit = display.textContent.slice(-1);
-			if (display.textContent.length === 1 && display.textContent === "0") {
-				display.textContent = this.value;
-				history.textContent = this.value;
-			} else if (isNaN(prevDigit) && prevDigit !== ".") {
+  // Evaluates history.textContent on display and rounds to the nearest hundredths
+  // if a decimal
+  equals.addEventListener("click", function () {
+    const lastIndex = display
+      .textContent
+      .slice(-1);
+    // If last digit is not a number then "Bad Syntax!" will display otherwise it
+    // will evaluate history.textContent
+    if (isNaN(lastIndex)) {
+      display.textContent = "Bad Syntax!";
+    } else {
+      // Rounds any decimal total to nearest hundredth
+      display.textContent = parseFloat(eval(history.textContent).toFixed(2));
+      history.textContent = parseFloat(eval(history.textContent).toFixed(2));
 
-				display.textContent = this.value;
-				history.textContent += this.value;
-
-			} else {
-				display.textContent += this.value;
-				history.textContent += this.value;
-			}
-			console.log(newNumber);
-			zeroState = true;
-			digitLimit();
-		});
-	}
-
-	// Iterates through operator elements and sets event listeners
-	for (var i = 0; i < operator.length; i++) {
-		operator[i].addEventListener("click", function () {
-			const prevIndex = display.textContent.slice(-1);
-			// if previous value is an operator, a new clicked operator will replace it
-			if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/") {
-				history.textContent += "";
-			} else {
-				display.textContent = this.value;
-				history.textContent += this.value;
-			}
-
-			decimalState = true;
-			zeroState = false;
-			digitLimit();
-		});
-	}
-
-	// clears display value
-	clear.addEventListener("click", function () {
-		init();
-	});
-
-	// deletes previous value on display
-	backSpace.addEventListener("click", function () {
-		display.textContent = "0";
-		history.textContent = history.textContent.slice(0, -1);
-
-		if (display.textContent.slice(-1) === ".") {
-			decimalState = true;
-		}
-
-	});
-
-	// adds decimal to display
-	decimal.addEventListener("click", function () {
-		const lastDigit = display.textContent.slice(-1);
-		// if decimal button is pressed, then it is disabled unless decimal in current integer is deleted
-		if (decimalState && isNaN(lastDigit)) {
-			display.textContent = "0" + this.value;
-			history.textContent += "0" + this.value;
-			decimalState = false;
-			zeroState = true;
-		} else if (decimalState) {
-			display.textContent += this.value;
-			history.textContent += this.value;
-			decimalState = false;
-			zeroState = true;
-		} else {
-			display.textContent += "";
-		}
-		digitLimit();
-	});
-
-	zero.addEventListener("click", function () {
-		// Prevents first number between operators from being zero
-		if (!zeroState) {
-			display.textContent += "";
-		} else {
-			display.textContent += this.value;
-			history.textContent += this.value;
-		}
-		digitLimit();
-	})
-
-	// Evaluates history.textContent on display and rounds to the nearest hundredths if a decimal
-	equals.addEventListener("click", function () {
-		const lastIndex = display.textContent.slice(-1);
-		// If last digit is not a number then "Bad Syntax!" will display otherwise it will evaluate history.textContent
-		if (isNaN(lastIndex)) {
-			display.textContent = "Bad Syntax!";
-
-		} else {
-			// Rounds any decimal total to nearest hundredth
-			display.textContent = parseFloat(eval(history.textContent).toFixed(2));
-			history.textContent = parseFloat(eval(history.textContent).toFixed(2));
-
-			decimalState = true;
-			console.log(history.textContent);
-		}
-	});
+      decimalState = true;
+      console.log(history.textContent);
+    }
+  });
 })()
