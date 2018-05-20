f1c399fcf93c7dbcac8be7d3880066dad39cc061
envincebal <vincentyan1@gmail.com>
2018-05-20 15:55:30 -0700

    Fixed formatting issues that broke entire app

--- js/app.js
+++ js/app.js
@@ -45,60 +45,50 @@
   // Iterates through number elements and sets event listeners
   for (var i = 0; i < number.length; i++) {
     number[i].addEventListener("click", function () {
-        const prevDigit = display
-          .textContent
-          .slice(-1);
-        if (display.textContent.length === 1 && display.textContent === "0") {
-          display.textContent = this.value;
-          history.textContent = this.value;
-        } else if (isNaN(prevDigit) && prevDigit !== ".") {
-          display.textContent = this.value;
-          history.textContent += this.value;
-        } else {
-          display.textContent += this.value;
-          history.textContent += this.value;
-        }
-        zeroState = true;
-        digitLimit();
-      });
+      const prevDigit = display.textContent.slice(-1);
+      if (display.textContent.length === 1 && display.textContent === "0") {
+        display.textContent = this.value;
+        history.textContent = this.value;
+      } else if (isNaN(prevDigit) && prevDigit !== ".") {
+        display.textContent = this.value;
+        history.textContent += this.value;
+      } else {
+        display.textContent += this.value;
+        history.textContent += this.value;
+      }
+      zeroState = true;
+      digitLimit();
+    });
   }
 
   // Iterates through operator elements and sets event listeners
   for (var i = 0; i < operator.length; i++) {
     operator[i].addEventListener("click", function () {
-        const prevIndex = display
-          .textContent
-          .slice(-1);
-        // if previous value is an operator, a new clicked operator will replace it
-        if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/") {
-          display.textContent = display
-            .textContent
-            .slice(0, -1);
-          history.textContent = history
-            .textContent
-            .slice(0, -1);
-          display.textContent += this.value;
-          history.textContent += this.value;
-        } else if (history.textContent.length > 0) {
-          display.textContent = this.value;
-          history.textContent += this.value;
-        }
-        decimalState = true;
-        zeroState = false;
-        digitLimit();
-      });
+      const prevIndex = display.textContent.slice(-1);
+      // if previous value is an operator, a new clicked operator will replace it
+      if (prevIndex === "*" || prevIndex === "+" || prevIndex === "-" || prevIndex === "/") {
+        display.textContent = display.textContent.slice(0, -1);
+        history.textContent = history.textContent.slice(0, -1);
+        display.textContent += this.value;
+        history.textContent += this.value;
+      } else if (history.textContent.length > 0) {
+        display.textContent = this.value;
+        history.textContent += this.value;
+      }
+      decimalState = true;
+      zeroState = false;
+      digitLimit();
+    });
   }
 
   // clears display value
   clear.addEventListener("click", function () {
-      init();
-    });
+    init();
+  });
 
   // adds decimal to display
   decimal.addEventListener("click", function () {
-    const lastDigit = display
-      .textContent
-      .slice(-1);
+    const lastDigit = display.textContent.slice(-1);
     // if decimal button is pressed, then it is disabled unless decimal in current
     // integer is deleted
     if (decimalState && isNaN(lastDigit)) {
@@ -131,9 +121,7 @@
   // Evaluates history.textContent on display and rounds to the nearest hundredths
   // if a decimal
   equals.addEventListener("click", function () {
-    const lastIndex = display
-      .textContent
-      .slice(-1);
+    const lastIndex = display.textContent.slice(-1);
     // If last digit is not a number then "Bad Syntax!" will display otherwise it
     // will evaluate history.textContent
     if (isNaN(lastIndex)) {
@@ -144,7 +132,6 @@
       history.textContent = parseFloat(eval(history.textContent).toFixed(2));
 
       decimalState = true;
-      console.log(history.textContent);
     }
   });
 })()
